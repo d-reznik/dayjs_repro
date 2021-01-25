ecb63814979f0c65546fab0b591341556a7d68b7
Dmitriy Reznik <dmr@global-mediator.com>
2021-01-25 11:29:52 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <MainPage/>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import MainPage from './components/MainPage.vue';
+
+@Options({
+  components: {
+    MainPage,
+  },
+})
+export default class App extends Vue {}
+</script>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{ currentTime }}</h1>    
+    <h1>{{ currentCopenhagenTime }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+import dayjs from 'dayjs';
+import timeZone from "dayjs/plugin/timezone";
+import isoWeek from "dayjs/plugin/isoWeek";
+dayjs.extend(timeZone);
+dayjs.extend(isoWeek);
+
+export default class MainPage extends Vue {
+  // This one works well
+  get currentTime(){
+    return dayjs();
+  }
+  // This one causes an error
+  get currentCopenhagenTime(){
+    return dayjs().tz("Europe/Copenhagen");
+  }
+}
+</script>
